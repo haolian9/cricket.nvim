2a40b584950f7803487ae1f798437fa4fde43ac0
haoliang <haoliang0.1.2@gmail.com>
2023-08-16 22:28:03 +0800

    rm api .collect, .play_this

--- lua/cricket/init.lua
+++ lua/cricket/init.lua
@@ -5,14 +5,13 @@ local bufrename = require("infra.bufrename")
 local coreutils = require("infra.coreutils")
 local ctx = require("infra.ctx")
 local Ephemeral = require("infra.Ephemeral")
-local ex = require("infra.ex")
 local fn = require("infra.fn")
 local fs = require("infra.fs")
 local handyclosekeys = require("infra.handyclosekeys")
 local highlighter = require("infra.highlighter")
 local jelly = require("infra.jellyfish")("cricket", "debug")
 local bufmap = require("infra.keymap.buffer")
-local prefer = require("infra.prefer")
+local popupgeo = require("infra.popupgeo")
 local strlib = require("infra.strlib")
 
 local player = require("cricket.player")
@@ -44,64 +43,15 @@ do
   end
 end
 
-local iter_music_files
-do
-  local allowed_exts = fn.toset({ ".mp3", ".m4a", ".flac" })
-
-  ---@param root string
-  ---@return fun(): string? @generates absolute file paths
-  function iter_music_files(root)
-    local iter = fn.filtern(function(fname, ftype)
-      if ftype ~= "file" then return false end
-      local ext = fs.suffix(fname)
-      if ext == nil then return false end
-      return allowed_exts[string.lower(ext)]
-    end, fs.iterdir(root))
-
-    return function()
-      local fname = iter()
-      if fname == nil then return end
-      return fs.joinpath(root, fname)
-    end
-  end
-end
-
----collect music files in a directory non-recursively
----@param dir string @absolute path
-function M.collect(dir)
-  if not fs.exists(dir) then return jelly.warn("%s not exists", dir) end
-
-  local path = fs.joinpath(facts.root, fs.basename(dir))
-
-  ex("tabedit", path)
-
-  if fs.exists(path) then return end
-
-  local bufnr = api.nvim_get_current_buf()
-  local lines = fn.tolist(iter_music_files(dir))
-  ctx.no_undo(bufnr, function() api.nvim_buf_set_lines(bufnr, 0, -1, false, lines) end)
-end
-
-function M.play_this()
-  local bufnr = api.nvim_get_current_buf()
-
-  local path = bufpath.file(bufnr)
-  if path == nil then return jelly.warn("no associated file with buf#%d", bufnr) end
-  if not strlib.startswith(path, facts.root) then return jelly.warn("not a known playlist") end
-
-  assert(player.playlist_switch(path))
-  jelly.info("playing: %s", fs.basename(path))
-end
-
 ---@param winid? integer
 function M.browse(winid)
-  if winid then
-    kite.land(facts.root)
-  else
-    kite.fly(facts.root)
-  end
+  if winid == nil then winid = api.nvim_get_current_win() end
+
+  kite.land(facts.root)
+
   local bufnr = api.nvim_get_current_buf()
   assert(strlib.startswith(api.nvim_buf_get_name(bufnr), "kite://"))
+
   bufmap(bufnr, "n", "<cr>", function()
     local fname = strlib.lstrip(api.nvim_get_current_line(), " ")
     assert(player.playlist_switch(fs.joinpath(facts.root, fname)))
@@ -110,12 +60,12 @@ function M.browse(winid)
 end
 
 do
-  local function get_playlist()
+  local function get_lines()
     return fn.tolist(fn.map(function(chirp) return fs.stem(chirp.filename) end, player.prop_playlist()))
   end
 
   local function rhs_reload(bufnr)
-    ctx.modifiable(bufnr, function() api.nvim_buf_set_lines(bufnr, 0, -1, false, get_playlist()) end)
+    ctx.modifiable(bufnr, function() api.nvim_buf_set_lines(bufnr, 0, -1, false, get_lines()) end)
   end
 
   local global_bufnr
@@ -123,7 +73,7 @@ do
   local function load_buf()
     if global_bufnr and api.nvim_buf_is_valid(global_bufnr) then return global_bufnr end
 
-    local bufnr = Ephemeral({ bufhidden = "hide" }, get_playlist())
+    local bufnr = Ephemeral({ bufhidden = "hide" }, get_lines())
     bufrename(bufnr, "cricket://controller")
 
     do
@@ -140,6 +90,7 @@ do
         player.play_index(index)
       end)
       bm.n("x", function()
+        --todo: fix the mysterious goto(0,0) when pressing q
         tui.confirm({ prompt = "quit the player?" }, function(confirmed)
           if not confirmed then return end
           assert(player.quit())
@@ -157,7 +108,7 @@ do
       bm.n("R", function() rhs_reload(bufnr) end)
     end
 
-    api.nvim_create_autocmd("winenter", {
+    api.nvim_create_autocmd({ "winenter", "bufwinenter" }, {
       buffer = bufnr,
       callback = function()
         assert(api.nvim_get_current_buf() == bufnr)
@@ -172,13 +123,9 @@ do
   function M.controller()
     local bufnr = load_buf()
 
-    do
-      local width = math.floor(vim.go.columns / 2)
-      local height = vim.go.lines - 2 - vim.go.cmdheight - 1 -- border + cmdline + statusline; tab?
-      local row, col = 0, 0
-      local winid = api.nvim_open_win(bufnr, true, { relative = "editor", border = "single", width = width, height = height, row = row, col = col })
-      api.nvim_win_set_hl_ns(winid, facts.floatwin_ns)
-    end
+    local width, height, row, col = popupgeo.editor_central(0.6, 0.8)
+    local winid = api.nvim_open_win(bufnr, true, { relative = "editor", border = "single", width = width, height = height, row = row, col = col })
+    api.nvim_win_set_hl_ns(winid, facts.floatwin_ns)
   end
 end
 
@@ -189,32 +136,25 @@ do
     local full = player.prop_filename()
     if full == nil then return "n/a" end
     local stem = fs.stem(full)
-    return string.match(stem, "^%d*[- .]*(.+)")
+    return assert(string.match(stem, "^%d*[- .]*(.+)"))
   end
 
   local feedfile = fs.joinpath(vim.fn.stdpath("state"), "cricket.obs")
 
-  local function op_feed()
-    uv.timer_start(timer, 0, 5 * 1000, function()
-      local file = assert(io.open(feedfile, "w"))
-      local ok, err = pcall(function() file:write(resolve_track_name()) end)
-      file:close()
-      if not ok then error(err) end
-    end)
-  end
-
-  local function op_stop() uv.timer_stop() end
+  local handlers = {
+    feed = function()
+      uv.timer_start(timer, 0, 5 * 1000, function()
+        local file = assert(io.open(feedfile, "w"))
+        local ok, err = pcall(function() file:write(resolve_track_name()) end)
+        file:close()
+        if not ok then error(err) end
+      end)
+    end,
+    stop = function() uv.timer_stop() end,
+  }
 
   ---@param op 'feed'|'stop'
-  function M.obs(op)
-    if op == "feed" then
-      op_feed()
-    elseif op == "stop" then
-      op_stop()
-    else
-      error("unknown op")
-    end
-  end
+  function M.obs(op) assert(handlers[op])() end
 end
 
 return M
