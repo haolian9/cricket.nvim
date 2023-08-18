d227b68da388a4a17eb51247e0dfc44c33078f41
haoliang <haoliang0.1.2@gmail.com>
2023-08-18 18:28:02 +0800

    fixup! prune scratch files

--- build.zig
+++ build.zig
@@ -20,18 +20,4 @@ pub fn build(b: *std.build.Builder) void {
         const test_step = b.step("test", "Run library tests");
         test_step.dependOn(&main_tests.step);
     }
-
-    const exe = b.addExecutable("simple", "src/simple.zig");
-    {
-        exe.setBuildMode(mode);
-        exe.linkLibC();
-        exe.linkSystemLibrary("mpv");
-        exe.strip = false;
-        exe.install();
-    }
-
-    {
-        const run_step = b.step("run", "run simple");
-        run_step.dependOn(&exe.run().step);
-    }
 }
