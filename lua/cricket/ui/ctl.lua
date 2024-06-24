1ee043647359e6a677f97f66ba3ac8d18e0d879e
haoliang <haoliang0.1.2@gmail.com>
2024-06-24 10:21:25 +0800

    sync with upstream

--- lua/cricket/ui/ctl.lua
+++ lua/cricket/ui/ctl.lua
@@ -47,7 +47,7 @@ do
         if player.playlist_current() ~= path then return jelly.info("no reloading as %s is not the current playlist", fs.basename(path)) end
 
         local same = itertools.equals(
-          itertools.project("filename", player.prop_playlist()),
+          itertools.project(player.prop_playlist(), "filename"),
           --
           buflines.iter(self.bufnr)
         )
